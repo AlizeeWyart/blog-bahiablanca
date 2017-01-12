70f8c5d992b9be7fedd6c18385625e81b8ee7f9d
Alizee Wyart <alizee.wyart@gmail.com>
2017-01-12 01:38:56 +0100

    logs

--- app/models/article.rb
+++ app/models/article.rb
@@ -8,6 +8,7 @@ class Article < ApplicationRecord
   has_many :region4as
   has_many :regions, through: :region4as
   has_many :comments
+  has_many :contents
   has_many :category4as
   has_many :categories, through: :category4as
 end
